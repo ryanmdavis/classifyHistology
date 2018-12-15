fixed rotation, improved border extraction

application/classify_tissue_border.py
@@ -1,9 +1,18 @@
+# 1) return the whole smoothed image border (not just the image locations) and the border index
+# corresponding to the image locations
+# 
+# 2) why is the border only covering a small segment of the surface?
+
 from classifyHistology.train_net import vars_phs_consts_metrics as vars
+from classifyHistology.train_net import functions as func
 from classifyHistology.extract_images import rw_images as extract
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import imageio
+import math,cmath,sys
 
-augmentation_hyperparameters={
-    'border_step':50,                   # number of pixels to step along tissue border before capturing the next image
+ah={
+    'border_step':200,                   # number of pixels to step along tissue border before capturing the next image
     'train_image_size_rc':[100,200],
     'rotate_deg':[0],
     'translate_pix_rc':[0],
@@ -13,7 +22,9 @@ augmentation_hyperparameters={
     'image_fill_factor':2/3, #must by <1, >0
     'im_downscale_factor':3,
     'test_dataset_size':0.2, #20% of data will go into test dataset
-    'norm_vec_len_px':50
+    'norm_vec_len_px':50,
+    'threshold_blue':200,
+    'strel_size':10
     }
 
 # training hyperparameteers
@@ -27,8 +38,9 @@ th = {
     'dropout_keep_prob': 0.5}
 
 # load the images to classify
-image_location='/media/ryan/002E-0232/nanozoomer_images/testing_application_Patient00-'
-normal_angle_rad_list,image_pos_rc_list,images_to_classify=extract.rwImages(image_location,augmentation_hyperparameters,to_mem=True)
+# image_location='/media/ryan/002E-0232/nanozoomer_images/Application_Data/large_dataset/Patient001'
+image_location='/media/ryan/002E-0232/nanozoomer_images/Application_Data/patient180-tumor1-tr-3-test'
+normal_angle_rad_list,image_pos_rc_list,images_to_classify,f_path=extract.rwImages(image_location,ah,to_mem=True)
 images_to_classify=images_to_classify[:,:th['n_input'][0],:th['n_input'][1],:th['n_input'][2]]
 
 # set up the graph, variables, and predictor
@@ -38,7 +50,7 @@ tf.reset_default_graph()
 weights,biases,x,y,ph_is_training = vars.definePhVar(th)
 
 # define performance metrics and optimizer
-_, _, _, pred = vars.performanceMetrics(x,y,weights,biases,th,False)
+pred = vars.performanceMetrics(x,y,weights,biases,th,False)[3] #predictor is 4th output in tuple
 
 # create the op to save and resotre all the variables
 saver = tf.train.Saver()
@@ -51,6 +63,27 @@ with tf.Session() as sess:
     saver.restore(sess, model_path)
     
     prediction = sess.run(pred, feed_dict={x: images_to_classify})
-    print(weights)
-    print(biases)
-print('test')
+    probs=tf.nn.softmax(prediction).eval()
+    print(probs)
+
+# read and plot image
+im_in_loc=f_path
+im=imageio.imread(im_in_loc)
+plt.imshow(im)
+
+# find the path corresponding to the surface of the image
+imag_vec_rc=[(cmath.exp(1j*normal_angle_rad_list[x]).imag,cmath.exp(1j*normal_angle_rad_list[x]).real) for x in range(len(normal_angle_rad_list))]
+annotation_pos_row=[int(imag_vec_rc[x][0]*ah['norm_vec_len_px']+image_pos_rc_list[x][0]) for x in range(len(imag_vec_rc))]
+annotation_pos_col=[int(imag_vec_rc[x][1]*ah['norm_vec_len_px']+image_pos_rc_list[x][1]) for x in range(len(imag_vec_rc))]
+image_surface_row=[image_pos_rc_list[x][0] for x in range(len(image_pos_rc_list))]
+image_surface_col=[image_pos_rc_list[x][1] for x in range(len(image_pos_rc_list))]
+plt.subplot('211')
+plt.imshow(im)
+plt.plot(annotation_pos_col,annotation_pos_row)
+plt.plot(image_surface_col,image_surface_row)
+plt.subplot('212')
+plt.plot(probs[:,0])
+
+# find positions of line annotations
+write_loc=f_path[0:f_path.rfind('/')+1]+'annotated-'+f_path[f_path.rfind('/')+1:]
+plt.savefig(write_loc)

extract_images/get_tissue_border.py
@@ -1,13 +1,14 @@
 from skimage.morphology import erosion,dilation,closing,opening, disk
 import matplotlib.pyplot as plt
 import numpy as np
-def getTissueBorder(bin_img):
-    strel5 = disk(5)
+def getTissueBorder(bin_img,ah):
+    strel5 = disk(ah['strel_size'])
     strel1 = disk(1)
     
     # smooth the edge
-    opened = opening(bin_img, strel5) #smoothes the edges a little bit so we get nice 1-pixel-wide paths
-    closed = closing(opened,strel5)
+    closed = closing(bin_img,strel5)
+    opened = opening(closed, strel5) #smoothes the edges a little bit so we get nice 1-pixel-wide paths
+
     
     # erode and calculate the tissue border image
     eroded = erosion(closed, strel1)
@@ -48,6 +49,116 @@ def getPixelPath(edge_image):
         row.append(row[-1]+drow)
         col.append(col[-1]+dcol)
         edge_image[row[-1],col[-1]]=False
+        #if ((row[-1]==616) and (col[-1]==1484)):
+        #    print('test')
+        image3x3, edge_image=getImage3x3(edge_image,row,col)
+        #image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
+    
+    return edge_image,row,col
+
+def getPixelPath2(edge_image):
+    
+    #we want to find an initial pixel that is one pixel removed from the edge,
+    # so that we can trance it from edge to edge
+ 
+    # set all edge pixels on the edge of the image as zero, since we need a 3x3
+    edge_image=removeBorderPixels(edge_image)
+
+    # get an initial pixel that is one away from the border
+    initial_pixel=getEdgePixelLoc(edge_image)
+    edge_image[initial_pixel[0],initial_pixel[1]]=False    
+    
+    edge_image,row,col=tracePath(edge_image,initial_pixel)
+    return edge_image,row,col
+    
+def tracePath(edge_image,initial_pixel):
+    pixel_locator_col=np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    pixel_locator_row=np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+    
+    row=[initial_pixel[0]]
+    col=[initial_pixel[1]]
+    current_pixel=initial_pixel
+    
+
+    image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
+
+        
+
+    #now calculate the location of the next pixel in the string        
+    while np.sum(image3x3)==1 & ~(row[-1]==0) & ~(col[-1]==0) & (np.sum(image3x3.shape) == 6):
+        drow=np.sum(image3x3*pixel_locator_row)
+        dcol=np.sum(image3x3*pixel_locator_col)
+        row.append(row[-1]+drow)
+        col.append(col[-1]+dcol)
+        edge_image[row[-1],col[-1]]=False
+        #if ((row[-1]==616) and (col[-1]==1484)):
+        #    print('test')
+        image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
+
+    # here test if image3x3.sum>2 and if so call tracepath in for loop. keep longest path
+    longest_row=[]
+    longest_col=[]
+    if image3x3.sum()>1:
+        nonzero=image3x3.nonzero()
+        path_max_dim=0
+        # now recursively call tracePath for each possible starting point in image3x3
+        for init_row,init_col in zip(nonzero[0],nonzero[1]):
+            new_edge_image=edge_image[:]
+            initial_pixel[0]=row[-1]+init_row-1
+            initial_pixel[1]=col[-1]+init_col-1
+            new_edge_image[initial_pixel[0],initial_pixel[1]]=False
+            new_edge_image,new_path_row,new_path_col=tracePath(new_edge_image,initial_pixel) #feed nonzeros into initial_pixel
+            # for loop here
+            # the right border has the longest diameter
+
+            path_width = np.max(new_path_col)-np.min(new_path_col)
+            path_height = np.max(new_path_row)-np.min(new_path_row)
+            new_path_max_dim = np.sqrt(path_width**2+path_height**2)
+            
+            if new_path_max_dim>path_max_dim:
+                pixel_path_row = new_path_row
+                pixel_path_col = new_path_col
+                edge_image=new_edge_image
+                path_max_dim=new_path_max_dim
+                
+        row = row+pixel_path_row
+        col = col+pixel_path_col    
+    return edge_image,row,col
+
+def getPixelPath3(edge_image):
+    
+    # find an initial pixel to start to calculate a string of pixels
+    # But don't let it be on the edge otherwise the 3x3 will reference an invalid pixel
+    # Only consider edge_image edges that make it to the edge of the image since
+    # those will be the actual edges that we care about
+
+    # set all edge pixels on the edge of the image as zero, since we need a 3x3
+    edge_image=removeBorderPixels(edge_image)
+
+    # get an initial pixel that is one away from the border
+    initial_pixel=getEdgePixelLoc(edge_image)
+    edge_image[initial_pixel[0],initial_pixel[1]]=False            
+    
+    pixel_locator_col=np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+    pixel_locator_row=np.array([[-1,-1,-1],[0,0,0],[1,1,1]])
+    
+    row=[initial_pixel[0]]
+    col=[initial_pixel[1]]
+    current_pixel=initial_pixel
+    
+    
+#     image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
+    image3x3, edge_image=getImage3x3(edge_image,row,col)
+        
+    #now calculate the location of the next pixel in the string        
+    while np.sum(image3x3) & ~(row[-1]==0) & ~(col[-1]==0) & (np.sum(image3x3.shape) == 6):
+        drow=np.sum(image3x3*pixel_locator_row)
+        dcol=np.sum(image3x3*pixel_locator_col)
+        row.append(row[-1]+drow)
+        col.append(col[-1]+dcol)
+        edge_image[row[-1],col[-1]]=False
+        #if ((row[-1]==616) and (col[-1]==1484)):
+        #    print('test')
         image3x3, edge_image=getImage3x3(edge_image,row,col)
         #image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
     
@@ -61,3 +172,34 @@ def getImage3x3(edge_image,row,col):
         edge_image[row[-1]-1+np.nonzero(image3x3)[0][0],col[-1]-1+np.nonzero(image3x3)[1][0]]=False
         image3x3=edge_image[row[-1]-1:row[-1]+2,col[-1]-1:col[-1]+2]
     return image3x3, edge_image
+
+def removeBorderPixels(edge_image):
+    edge_image[:,0]=False
+    edge_image[:,edge_image.shape[1]-1]=False
+    edge_image[0,:]=False
+    edge_image[edge_image.shape[0]-1,:]=False
+    return edge_image
+
+# get a pixel with value true that is one pixel away from the border of edge_image
+def getEdgePixelLoc(edge_image):
+    if (edge_image[1,:]==True).sum():
+        col_nz=edge_image[1,:].nonzero()[0]
+        edge_col=col_nz[0]
+        edge_row=1
+    elif (edge_image[edge_image.shape[0]-2,:]==True).sum():
+        col_nz=edge_image[edge_image.shape[0]-2,:].nonzero()[0]
+        edge_col=col_nz[0]
+        edge_row=edge_image.shape[0]-2
+    elif (edge_image[:,1]==True).sum():
+        row_nz=edge_image[:,1].nonzero()[0]
+        edge_row=row_nz[0]
+        edge_col=1
+    elif (edge_image[:,edge_image.shape[1]-2]==True).sum():
+        row_nz=edge_image[:,edge_image.shape[1]-2].nonzero()[0]
+        edge_row=row_nz[0]
+        edge_col=edge_image.shape[1]-2
+    else:
+        edge_row=-1
+        edge_col=-1
+    
+    return [edge_row,edge_col]  

offline_augmentation/augment_and_save.py
@@ -30,7 +30,7 @@ def borderWalk(image,im_thresh,row_border,col_border,ah):
         while not isSubImageInvalid(border_index,row_border,col_border,aug_image_half_width,image.shape,ah['norm_vec_len_px']):
       
             # calculate the orientation of the image, i.e., the angle of the normal vector wrt the image axis
-            normal_angle_rad = calcNormalAngle(im_thresh,row_border,col_border,border_index,half_bs,ah['norm_vec_len_px'])
+            normal_angle_rad = calcNormalAngle(im_thresh,row_border,col_border,border_index,max([half_bs,10]),ah['norm_vec_len_px'])
       
             # grab the aug_temp_image. It's bigger than needed because we need to rotate
             x_min=col_border[border_index]-aug_image_half_width
@@ -67,10 +67,11 @@ def augmentAndSave(image,normal_angle_rad,fname,dir_name,ah,save_dir='/home/ryan
     x_shift=int(np.around(np.real(ah['train_image_size_rc'][1]*adjust*np.exp(normal_angle_rad*1j))))
     y_shift=int(np.around(np.imag(ah['train_image_size_rc'][0]*adjust*np.exp(normal_angle_rad*1j))))
     
-    for rot_ang in ah['rotate_deg']-normal_angle_deg:
+    for rot_ang_list in ah['rotate_deg']:
+        rot_ang=rot_ang_list-normal_angle_deg
         # rotate image so surface is parallel to columns (plus the data augmentation rotation) and scale
         # so that maximum pixel value is 1.
-        rotated = sp.ndimage.rotate(image,rot_ang)
+        rotated = sp.ndimage.rotate(image,-rot_ang)
         
         # we shift the image because we want most of the image to be compose of tissue
         # note this shift assumes that the image has been rotated (ignoring augmentation rotation)